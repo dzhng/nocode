658521f325aaa69b91f78494eb555e7544014f87
David Zhang <dzz0615@gmail.com>
2021-06-28 23:29:38 +0700

    layout logic

--- modules/app/src/pages/index.tsx
+++ modules/app/src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { makeStyles, createStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
-const useStyles = makeStyles(() =>
+const NavBarHeight = 40;
+
+const useStyles = makeStyles((theme) =>
   createStyles({
     container: {
       width: '100%',
@@ -8,12 +11,34 @@ const useStyles = makeStyles(() =>
     },
     navBar: {
       width: '100%',
-      height: 50,
+      borderBottom: theme.dividerBorder,
+      height: NavBarHeight,
     },
     logo: {
-      height: 40,
-      marginTop: 5,
-      marginBottom: 5,
+      height: NavBarHeight - 10,
+      margin: 5,
+    },
+    content: {
+      height: `calc(100% - ${NavBarHeight}px)`,
+      display: 'flex',
+    },
+    section: {
+      height: '100%',
+    },
+    center: {
+      flexGrow: 1,
+    },
+    left: {
+      flexGrow: 0,
+      flexShrink: 0,
+      width: 100,
+      backgroundColor: '#CCC',
+    },
+    right: {
+      flexGrow: 0,
+      flexShrink: 0,
+      width: 200,
+      backgroundColor: '#CCC',
     },
   }),
 );
@@ -25,6 +50,11 @@ export default function Home() {
     <div className={classes.container}>
       <div className={classes.navBar}>
         <img className={classes.logo} src="/logo.png" />
+      </div>
+      <div className={classes.content}>
+        <div className={clsx(classes.left, classes.section)}></div>
+        <div className={clsx(classes.center, classes.section)}></div>
+        <div className={clsx(classes.right, classes.section)}></div>
       </div>
     </div>
   );
