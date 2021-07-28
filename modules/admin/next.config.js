d1895db42949c6b063e15c0511f8244caccef02d
David Zhang <dzz0615@gmail.com>
2021-07-28 11:11:10 +0700

    rearranged modules

--- modules/admin/next.config.js
+++
@@ -1,7 +0,0 @@
-const withTM = require('next-transpile-modules')(['nocode-shared']); // pass the modules you would like to see transpiled
-
-module.exports = withTM({
-  images: {
-    domains: ['firebasestorage.googleapis.com', 'v2.convertapi.com'],
-  },
-});
