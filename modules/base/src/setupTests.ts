a13b74c8d593648a49755ba62d6470773d55e11a
David Zhang <dzz0615@gmail.com>
2021-08-09 00:00:35 +0700

    removed firebase files, moved useworkspace

--- modules/base/src/setupTests.ts
+++ modules/base/src/setupTests.ts
@@ -2,13 +2,6 @@ import '@testing-library/jest-dom';
 import 'isomorphic-unfetch';
 import 'jest-ts-auto-mock';
 
-// Mocks the Fullscreen API. This is needed for ToggleFullScreenButton.test.tsx.
-// Guard in if statement for any tests that specifies node environment (e.g. firebase)
-// TODO: just move this to local file, currently fails test for some reason
-if (global.document) {
-  Object.defineProperty(document, 'fullscreenEnabled', { value: true, writable: true });
-}
-
 if (global.window) {
   class LocalStorage {
     store = {} as { [key: string]: string };
