14f478aee1e39621992adb24f1208ed3e99e261c
David Zhang <dzz0615@gmail.com>
2021-08-08 11:24:44 +0700

    removed more firebase stuff

--- modules/firebase/src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const isDev = process.env.NODE_ENV !== 'production';
-export const DefaultRegion = 'asia-east2';
--- modules/firebase/src/index.ts
+++
@@ -1,6 +0,0 @@
-import * as functions from 'firebase-functions';
-import 'firebase-functions';
-
-export const helloWorld = functions.https.onRequest(async (_, res) => {
-  res.send('<p>hello world!</p>');
-});
