0fbde409e2a762205c569da56d0d816bef5d43ef
David Zhang <dzz0615@gmail.com>
2021-09-12 21:42:53 +0700

    added schema for column types

--- modules/shared/schema/index.ts
+++ modules/shared/schema/index.ts
@@ -59,3 +59,88 @@ export declare interface App {
   isDeleted: boolean;
   createdAt: Date;
 }
+
+export declare interface Sheet {
+  id?: number;
+  appId: number;
+  name: string;
+  order: number;
+  columns: ColumnType[];
+  isDeleted: boolean;
+  createdAt: Date;
+}
+
+export enum DataTypes {
+  Text = 'text',
+  Image = 'image',
+  Number = 'number',
+  File = 'file',
+  Date = 'date',
+  Location = 'location',
+
+  // select from a list of predefined text or numbers
+  SingleSelection = 'single_selection',
+  MultipleSelection = 'multiple_selection',
+
+  // can be text, image, number, file, date, or location
+  List = 'list',
+
+  // map to value from another table, based on a value from this row
+  RelationOne = 'relation_one',
+  RelationMany = 'relation_many',
+}
+
+/**
+ * Column definitions
+ */
+
+export interface SelectionMeta {
+  type: DataTypes.Text | DataTypes.Number;
+  // all potiential options that can be selected
+  options: string[] | number[];
+}
+
+export interface ListMeta {
+  // the type of data that is stored in the list
+  type: DataTypes;
+}
+
+export interface RelationMeta {
+  // the column in the current table that serves as the key
+  keyColumnId: string;
+
+  // point to the location of the relational data value
+  valueTableId: string;
+  valueColumnId: string;
+}
+
+export interface TableMeta {
+  width?: number;
+  isHidden?: boolean;
+}
+
+export interface ColumnType {
+  columnId: string;
+  name: string;
+  type: DataTypes;
+  defaultValue?: CellType;
+  typeMetadata?: SelectionMeta | ListMeta | RelationMeta;
+  tableMetadata?: TableMeta;
+}
+
+/**
+ * Row and Cell definitions
+ */
+
+export type CellType = string | number | Date | object | string[] | number[];
+
+export interface Record {
+  id?: number;
+  sheetId: number;
+  order: number;
+  data: {
+    [columnID: string]: CellType | undefined;
+  };
+  createdAt: Date;
+  modifiedAt: Date;
+}
