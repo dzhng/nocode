e5b0a2814b51bf98d0959a7b601a203998d98dad
David Zhang <dzz0615@gmail.com>
2021-09-22 00:23:53 +0700

    sheet loading logic

--- modules/base/src/containers/Sheet/index.tsx
+++ modules/base/src/containers/Sheet/index.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import clsx from 'clsx';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { Sheet } from 'shared/schema';
 import useApp from '~/hooks/useApp';
 import DataTable from '~/components/DataTable';
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme) =>
   createStyles({
     content: {
       height: '100%',
@@ -16,24 +17,38 @@ const useStyles = makeStyles(() =>
     },
     tabContainer: {
       flexShrink: 0,
-      height: 40,
-      background: '#DEDEDE',
+      height: 30,
       display: 'flex',
       flexDirection: 'row',
     },
     tab: {
-      height: 40,
-      width: 200,
-      background: '#CCC',
+      width: 100,
+      lineHeight: '28px',
+      textAlign: 'center',
+      background: '#DDD',
+      margin: 2,
+      borderRadius: theme.shape.borderRadius,
       flexShrink: 0,
+      cursor: 'pointer',
+
+      '&.selected': {
+        background: '#CCC',
+      },
     },
   }),
 );
 
 export default function SheetContainer({ appId }: { appId: number }) {
   const classes = useStyles();
-  const { sheets, createSheet /*, isLoadingSheets*/ } = useApp(appId);
+  const { sheets, createSheet, isLoadingSheets } = useApp(appId);
   const [selectedSheet, setSelectedSheet] = useState<Sheet | null>(null);
+
+  // select the first sheet once it loads
+  useEffect(() => {
+    if (!isLoadingSheets && sheets.length > 0) {
+      setSelectedSheet(sheets[0]);
+    }
+  }, [isLoadingSheets, sheets]);
 
   return (
     <div className={classes.content}>
@@ -42,7 +57,11 @@ export default function SheetContainer({ appId }: { appId: number }) {
       </div>
       <div className={classes.tabContainer}>
         {sheets.map((sheet) => (
-          <div key={sheet.id} className={classes.tab} onClick={() => setSelectedSheet(sheet)}>
+          <div
+            key={sheet.id}
+            className={clsx(classes.tab, selectedSheet === sheet && 'selected')}
+            onClick={() => setSelectedSheet(sheet)}
+          >
             {sheet.name}
           </div>
         ))}
