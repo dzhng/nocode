cea0546b212dbab9185274d4226473c3eb9d9423
David Zhang <dzz0615@gmail.com>
2021-09-25 19:13:40 +0700

    simplified row and cell styling

--- modules/base/src/components/DataTable/index.tsx
+++ modules/base/src/components/DataTable/index.tsx
@@ -3,7 +3,10 @@ import { makeStyles, createStyles } from '@mui/styles';
 import { Box } from '@mui/material';
 import { Sheet, DataTypes } from 'shared/schema';
 import useSheet from '~/hooks/useSheet';
+import { AddIcon } from '~/components/Icons';
 import Cell from './Cell';
+import CellContainer from './CellContainer';
+import Row from './Row';
 
 const DefaultCellLength = 100;
 const DefaultCellHeight = 40;
@@ -15,29 +18,27 @@ const useStyles = makeStyles((theme) =>
       width: '100%',
       height: '100%',
     },
-    columnHeader: {
-      height: DefaultCellHeight,
-    },
-    rowContainer: {},
     row: {
       height: DefaultCellHeight,
-    },
-    cellContainer: {
-      display: 'inline-block',
-      width: DefaultCellLength,
-      height: '100%',
-      overflow: 'hidden',
-      borderColor: theme.palette.grey[300],
     },
   }),
 );
 
 function AddNewColumnHeader({ onClick }: { onClick(): void }) {
-  const classes = useStyles();
   return (
-    <div className={classes.cellContainer} onClick={onClick}>
-      +
-    </div>
+    <CellContainer
+      sx={{
+        width: DefaultCellLength,
+        backgroundColor: 'grey.100',
+        borderTopRightRadius: CellBorderRadius,
+        borderLeft: 'none',
+        textAlign: 'center',
+        '& svg': { width: 20, height: 20, mt: '9px', color: 'primary.main' },
+      }}
+      onClick={onClick}
+    >
+      <AddIcon />
+    </CellContainer>
   );
 }
 
@@ -87,21 +88,18 @@ export default function DataTable({ sheet }: { sheet: Sheet }) {
   }, []);
 
   return (
-    <div className={classes.container}>
-      <div className={classes.columnHeader}>
+    <Box className={classes.container}>
+      <Box sx={{ width: 'fit-content', height: DefaultCellHeight }}>
         {columns.map((column, columnIdx) => (
-          <Box
+          <CellContainer
             key={column.id}
-            className={classes.cellContainer}
             sx={{
+              width: DefaultCellLength,
+              height: DefaultCellHeight,
               fontWeight: 'bold',
               backgroundColor: 'grey.100',
-              borderLeft: columnIdx === 0 ? 1 : 0,
-              borderTop: 1,
-              borderBottom: 1,
-              borderRight: 1,
+              borderLeft: columnIdx === 0 ? undefined : 'none',
               borderTopLeftRadius: columnIdx === 0 ? CellBorderRadius : 0,
-              borderTopRightRadius: columnIdx === columns.length - 1 ? CellBorderRadius : 0,
             }}
           >
             <Cell
@@ -111,58 +109,26 @@ export default function DataTable({ sheet }: { sheet: Sheet }) {
                 changeColumn(column.id, { name: String(newData), type: column.type });
               }}
             />
-          </Box>
+          </CellContainer>
         ))}
         <AddNewColumnHeader onClick={onAddColumn} />
-      </div>
+      </Box>
 
-      <div className={classes.rowContainer}>
-        {records.map((record, recordIdx) => (
-          <div key={record.id ?? -1} className={classes.row}>
-            <Box
-              onMouseOver={() => onRowHover(record.id ?? -1)}
-              onMouseLeave={onRowHoverLeave}
-              sx={{
-                display: 'inline-block',
-                height: DefaultCellHeight,
-                bgcolor: (theme) => (hoveredRecord === record.id ? theme.hoverColor : 'white'),
-              }}
-            >
-              {columns.map((column, columnIdx) => (
-                <Box
-                  key={column.id}
-                  className={classes.cellContainer}
-                  sx={{
-                    borderLeft: columnIdx === 0 ? 1 : 0,
-                    borderBottom: 1,
-                    borderRight: 1,
-                    borderBottomLeftRadius:
-                      recordIdx === records.length - 1 && columnIdx === 0 ? CellBorderRadius : 0,
-                    borderBottomRightRadius:
-                      recordIdx === records.length - 1 && columnIdx === columns.length - 1
-                        ? CellBorderRadius
-                        : 0,
-                  }}
-                >
-                  <Cell
-                    column={column}
-                    data={record.id && cellForRecord(record.id, column.id)?.data}
-                    onChange={(newData) => {
-                      if (record.id && newData !== undefined) {
-                        editRecord(record.id, column.id, newData);
-                      }
-                    }}
-                  />
-                </Box>
-              ))}
-            </Box>
-          </div>
-        ))}
-      </div>
+      {records.map((record) => (
+        <Row
+          key={record.id ?? -1}
+          columns={columns}
+          width={DefaultCellLength}
+          isHovered={hoveredRecord === record.id}
+          onHover={() => onRowHover(record.id ?? -1)}
+          onHoverLeave={onRowHoverLeave}
+          defaultHeight={DefaultCellHeight}
+          dataForColumn={(columnId) => record.id && cellForRecord(record.id, columnId)?.data}
+          editRecord={(columnId, data) => record.id && editRecord(record.id, columnId, data)}
+        />
+      ))}
 
-      <div className={classes.rowContainer}>
-        <AddNewRow onClick={onAddRow} />
-      </div>
-    </div>
+      <AddNewRow onClick={onAddRow} />
+    </Box>
   );
 }
--- modules/base/src/components/Icons.tsx
+++ modules/base/src/components/Icons.tsx
@@ -1,7 +1,11 @@
-import { ArrowBackIosOutlined as BackIcon, AppsOutlined as AppsIcon } from '@mui/icons-material';
+import {
+  ArrowBackIosOutlined as BackIcon,
+  AppsOutlined as AppsIcon,
+  AddCircleOutline as AddIcon,
+} from '@mui/icons-material';
 
 // re-export standardized icons so they are consistent
-export { BackIcon, AppsIcon };
+export { BackIcon, AppsIcon, AddIcon };
 
 export const Logo = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
   // eslint-disable-next-line @next/next/no-img-element
